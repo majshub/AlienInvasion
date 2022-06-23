ad89231597d116768c8885439feb0c0554ea0144
Majid : 4005193022 <majidjj108@gmail.com>
2022-06-23 13:34:13 +0430

    v1 : creating pygame window

---
+++ alien_invasion.py
@@ -0,0 +1,34 @@
+import sys
+
+import pygame
+
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+    def __init__(self):
+        """Initialize the game , and create game resources."""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+        self.bg_color = (230, 230, 230)
+
+    def run_game(self):
+        """starts the main loop for the game."""
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            self.screen.fill(self.bg_color)
+
+            #makes the most recently drawn screen visible
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    #here we run the game
+    ai = AlienInvasion()
+    ai.run_game()
