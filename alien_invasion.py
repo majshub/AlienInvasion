4c86bbbe45986fa51e8debde42d162b02eb805cc
Majid : 4005193022 <majidjj108@gmail.com>
2022-06-23 14:01:51 +0430

    v4 : Refactoring (run_game)

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,7 @@ from settings import Settings
 
 from ship import Ship
 
+
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
     def __init__(self):
@@ -22,15 +23,19 @@ class AlienInvasion:
         """starts the main loop for the game."""
 
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            #makes the most recently drawn screen visible
-            pygame.display.flip()
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
