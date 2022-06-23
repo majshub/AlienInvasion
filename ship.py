2fbb8fdcf1240a4da1cc3e2cee1d666c4ad89a72
Majid : 4005193022 <majidjj108@gmail.com>
2022-06-23 18:56:57 +0430

    v6 : moving left and right

--- ship.py
+++ ship.py
@@ -13,10 +13,13 @@ class Ship:
         self.rect.midbottom = self.screen_rect.midbottom
 
         self.moving_right = False
+        self.moving_left = False
 
     def update(self):
         if self.moving_right:
             self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
 
     def blitme(self):
         """draw the  ship at its current location"""
