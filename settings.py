4739f5b30aaaec8092adac834f250cdec5e8b4b5
Majid : 4005193022 <majidjj108@gmail.com>
2022-06-23 13:47:22 +0430

    v2 : creating setting class

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings:
+    """stores the setting of the game"""
+
+    def __init__(self):
+        self.screen_width = 1300
+        self.screen_height = 650
+        self.bg_color = (230, 230, 230)
