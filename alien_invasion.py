b8d5bbec052929340fe8479f1a876344cb92d046
Majid : 4005193022 <majidjj108@gmail.com>
2022-07-09 20:36:07 +0430

    v15_3 : Deactivating the Play Button

--- alien_invasion.py
+++ alien_invasion.py
@@ -65,7 +65,8 @@ class AlienInvasion:
                 self._check_keyup_events(event)
 
     def _check_play_button(self, mouse_pos):
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
             self.stats.reset_stats()
             self.stats.game_active = True
 
