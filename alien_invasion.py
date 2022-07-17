4b581f9ad13edee4e247ac67dd085073c25c8b4b
Majid : 4005193022 <majidjj108@gmail.com>
2022-07-17 21:04:32 +0430

    v17_2 : Resetting the Score

--- alien_invasion.py
+++ alien_invasion.py
@@ -77,6 +77,7 @@ class AlienInvasion:
         self.settings.initialize_dynamic_settings()
         self.stats.reset_stats()
         self.stats.game_active = True
+        self.sb.prep_score()
 
         self.aliens.empty()
         self.bullets.empty()
